88ddab4e35b18f3b2680e1659a3cf92fbae6854d
Eloy Bermejo <eloybermejo@inspladelestany.cat>
2022-10-07 17:41:16 +0200

    Usuari

--- Usuari.java
+++ Usuari.java
@@ -1,5 +1,6 @@
 public class Usuari {
     private String nomDelUsuari;
+	//L'Eloy pensa que el Monkey Island està infravalorat
 
     public Usuari(String nomUsuari) {
         this.nomDelUsuari = nomUsuari;
